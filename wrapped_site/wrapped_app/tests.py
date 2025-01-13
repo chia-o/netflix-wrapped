56eff9a5ca53c10db23b247d726dcf3fcccd5f05
Chiamaka Ofonagoro <chiamakaofo@gmail.com>
2025-01-13 15:47:57 -0500

    Wrote a basic unittest but keep getting error related to one of my tables. I'll check test database and potentially rebuild migrations.

--- wrapped_site/wrapped_app/tests.py
+++ wrapped_site/wrapped_app/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+def add_two_numbers(a, b):
+    return a + b
+
+class TestExample(TestCase):
+    def test_add_two_numbers(self):
+        self.assertEqual(add_two_numbers(1, 2), 3)
