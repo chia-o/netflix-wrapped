bff97bbe1edbf065bdf73ffe018597f3fb23d052
Chiamaka Ofonagoro <chiamakaofo@gmail.com>
2025-02-08 20:59:15 -0500

    Removed genere field from Titles and re-added it.

--- wrapped_site/wrapped_app/models.py
+++ wrapped_site/wrapped_app/models.py
@@ -24,7 +24,7 @@ class Titles(models.Model):
     title_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255, blank=True, null=True)
     video_type = models.CharField(max_length=255, choices=[('Movie', 'Movie'), ('TV Show', 'TV Show')], blank=True, null=True, default=None)
-    genre = models.ManyToManyField('Genres', related_name='titles')
+    genre = models.ManyToManyField('Genres', related_name='titles', default=None) 
 
     class Meta:
         managed = True
